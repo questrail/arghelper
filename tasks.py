46b152e810c375160fe0ee4f01d9a8f8cf2d44e0
Matthew Rankin <matthew.d.rankin@gmail.com>
2014-06-24 15:05:01 -0500

    Modify invoke release command

--- tasks.py
+++ tasks.py
@@ -17,32 +17,24 @@ def test():
 
 
 @task()
-def release(start=False, finish=False, deploy=False, test=False, version=''):
+def release(deploy=False, test=False, version=''):
     """Release arghelper and deploy to PyPI
     """
     if test:
         run("python setup.py check")
         run("python setup.py register sdist upload --dry-run")
 
-    if start:
+    run("python setup.py check")
+    if deploy:
         if version:
-            run("git flow release start v{ver}".format(
-                ver=version))
-    if finish:
-        run("python setup.py check")
-        if deploy:
-            if version:
-                run("python setup.py sdist")
-                run("git flow release finish -m '{ver}' v{ver}".format(
-                    ver=version), echo=True)
-                run("git push --tags")
-                run("git checkout master")
-                run("python setup.py register sdist upload")
-                run("git checkout develop")
-                """Run flake8 to lint code"""
-        else:
-            print("* Have you updated the version in arghelper.py?")
-            print("* Have you updated CHANGES.md?")
-            print("* Have you fixed any last minute bugs?")
-            print("If you answered yes to all of the above questions,")
-            print("then run `invoke release --finish --deploy -vX.YY`")
+            run("python setup.py sdist")
+            # run("git push --tags")
+            # run("git checkout master")
+            run("python setup.py register sdist upload")
+            """Run flake8 to lint code"""
+    else:
+        print("* Have you updated the version in arghelper.py?")
+        print("* Have you updated CHANGES.md?")
+        print("* Have you fixed any last minute bugs?")
+        print("If you answered yes to all of the above questions,")
+        print("then run `invoke release --deploy -vX.YY`")
